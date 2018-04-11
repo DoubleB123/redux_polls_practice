b311ed988b8e6a80c20b93265803b3bbb90b09e8
DoubleB123 <bbrenner321@gmail.com>
2018-04-11 16:07:59 -0700

    inital commit

---
+++ src/middleware/logger.js
@@ -0,0 +1,11 @@
+const logger = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.log('Action:', action);
+  const returnVal = next(action);
+  console.log('New state: ', store.getState());
+  console.groupEnd();
+  return returnVal;
+}
+
+
+export default logger;
